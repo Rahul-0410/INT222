9209f0ca3bd249fecff96e8baf4aa5bba74f4f1d
Rahul-0410 <rgs786999@gmail.com>
2024-03-01 14:35:38 +0530

    express

--- express/test2.js
+++ express/test2.js
@@ -44,6 +44,24 @@ app.get("/home", (req,res)=>{
 app.get("/form",(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
+app.get("/formpro",(req,res)=>{
+    res.send(
+        {
+        name:req.query.name,
+        Number:req.query.num,
+        email:req.query.email,
+        gender:req.query.gender,
+        password:req.query.password
+    }
+    // '<p> first name'+req.query['name'] + req.query['num']+'</p>'
+    )
+})
+// app.get("/formpro",(req,res)=>{
+//     res.send({
+//         firstname:req.query.fname,
+//         lastname:req.query.lname
+//     })
+// })
 
 
 app.listen(3000, ()=>{
